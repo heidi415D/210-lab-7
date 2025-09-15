95b76b0cf4fd8995d1ecc3402ebd527da2078ea2
1080heidi <heidideleon415@gmail.com>
2025-09-14 17:46:36 -0700

    Second Update

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -7,19 +7,45 @@ using namespace std;
 
 const int SIZE = 5; 
 
+// function to reverse the array
 string * reverseArray(string* arr, int size){
     for(int i = 0; i<= size/2; i++){
         string temp = *(arr + i);
         *(arr + i) = *(arr + (size - 1 - i));
         *(arr + (size - 1 - i)) = temp;
     }
-    return arr;
+    return arr; // return the pointer to the reversed array
 }
 
+// function to display the array
 void displayArray(string* arr, int size){
     cout << "Outputting array elements: ";
     for(int i = 0; i < size; i++){
         cout << *(arr + i) << " ";
     }
     cout << endl;
+}
+
+int main(){
+    string* names = new string[SIZE]; // dynamically allocate array
+
+    *(names + 0) = "Janet";
+    *(names + 1) = "Jeffe";
+    *(names + 2) = "Jin";
+    *(names + 3) = "Joe";
+    *(names + 4) = "Junio";
+
+   // output original
+    cout << "Original array: " << endl;
+    displayArray(names, SIZE);
+
+    // reverse the array
+    names = reverseArray(names, SIZE);
+
+    // output reversed
+    cout << "Reversed array: " << endl;
+    displayArray(names, SIZE);
+
+    return 0;
+
 }
