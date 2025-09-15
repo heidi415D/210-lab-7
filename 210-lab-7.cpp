b134f9f662529191338a32c78f1cd1adca3e034c
1080heidi <heidideleon415@gmail.com>
2025-09-14 17:33:40 -0700

    First Update

--- 210-lab-7.cpp
+++ 210-lab-7.cpp
@@ -2,4 +2,24 @@
 // IDE USED: VS CODE
 
 #include <iostream>
-#include <>
+#include <string>
+using namespace std;
+
+const int SIZE = 5; 
+
+string * reverseArray(string* arr, int size){
+    for(int i = 0; i<= size/2; i++){
+        string temp = *(arr + i);
+        *(arr + i) = *(arr + (size - 1 - i));
+        *(arr + (size - 1 - i)) = temp;
+    }
+    return arr;
+}
+
+void displayArray(string* arr, int size){
+    cout << "Outputting array elements: ";
+    for(int i = 0; i < size; i++){
+        cout << *(arr + i) << " ";
+    }
+    cout << endl;
+}
